65c9427cf6aaf74597b1a695d56861541335717a
Vo Phi Long <long.vo-phi@internal.banvien.com>
2022-04-06 13:09:33 +0700

    fix new button

--- src/App.js
+++ src/App.js
@@ -14,14 +14,11 @@ function App() {
         <div className="primary-button">
           <p>Secondary</p>
           <Button $type="Secondary" $width="80%" $size="30px">Default</Button>
-         
-      
         </div>
 
         <div className="primary-button">
             <p>Gray</p>
             <Button $type="Gray" $width="100%" $size="30px">Default</Button>
-            
         </div>
 
       </div>
